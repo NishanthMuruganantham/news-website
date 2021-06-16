a7f1a69d9be39d84ddbc68a30bb4c9327cfdfc23
NishanthMuruganantham <nishanthmurugananth10@gmail.com>
2021-06-17 01:15:45 +0530

    headlines link has been added, Homepage feed is linked

--- news_project/news_apps/core.py
+++ news_project/news_apps/core.py
@@ -6,17 +6,18 @@ core = Blueprint('core', __name__)
 @core.route('/')
 def index():
     uri = "https://ndtvnews-api.herokuapp.com/{}?{}=values({})"
+    home_latest_news = fetch_general_news(uri = uri.format("general","category","latest"))
     home_india_news = fetch_general_news(uri = uri.format("general","category","india"))
     home_world_news = fetch_general_news(uri= uri.format("general","category","world"))
     home_business_news = fetch_general_news(uri = uri.format("general","category","business"))
-    # home_football_news = fetch_general_news(uri = uri.format("sports","sport","football"))
-    # home_cricket_news = fetch_general_news(uri = uri.format("sports","sport","cricket"))
-    # home_tennis_news = fetch_general_news(uri = uri.format("sports","sport","tennis"))
-    return render_template("index.html",home_india_news = home_india_news,
-                        home_world_news = home_world_news, home_business_news = home_business_news,)
-                        # home_football_news= home_football_news,
-                        # home_cricket_news = home_cricket_news,
-                        # home_tennis_news = home_tennis_news)
+    home_football_news = fetch_general_news(uri = uri.format("sports","sport","football"))
+    home_cricket_news = fetch_general_news(uri = uri.format("sports","sport","cricket"))
+    home_tennis_news = fetch_general_news(uri = uri.format("sports","sport","tennis"))
+    return render_template("index.html",home_latest_news = home_latest_news, home_india_news = home_india_news,
+                        home_world_news = home_world_news, home_business_news = home_business_news,
+                        home_football_news= home_football_news,
+                        home_cricket_news = home_cricket_news,
+                        home_tennis_news = home_tennis_news)
 
 @core.app_errorhandler(404)
 def page_not_found(e):
